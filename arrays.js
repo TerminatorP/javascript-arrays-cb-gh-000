5f4a636670bd0b43ed8bc966a8b6f897ad634804
Prabesh Subedi <subediprabesh1@gmail.com>
2018-04-09 03:39:27 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,28 +1,5 @@
-const chocolateBars = [
-  'snickers',
-  'hundred grand',
-  'kitkat',
-  'skittles'
-];
-
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray(arr, el) {
-  let outputArr = arr;
-  return outputArr.unshift(el);
-}
-
-function destructivelyAddElementToBeginningOfArray(arr, el) {
-  arr.unshift(el);
-  return arr;
-}
-function addElementToEndOfArray(arr, el) {
-  let outputArr = arr;
-  outputArr.push(el);
-  return outputArr;
-}
-function destructivelyAddElementToEndOfArray(arr, el) {
-  arr.push(el);
-  return arr;
-}
-function accessElementInArray(arr, i) {
-  return arr[i];
+  let newArr = [...arr];
+  return newArr.unshift(el);
 }
